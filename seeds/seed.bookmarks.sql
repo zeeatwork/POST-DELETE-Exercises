e85085e8c6c317e1f1281e37abb6d5d74e70d24e
Zenzi Ali <zeeathome935@gmail.com>
2020-08-23 20:59:36 -0400

    with testing

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,5 @@
+INSERT INTO bookmarks (title, url, rating, description)
+VALUES
+  ('Twitter', 'https://www.twitter.com', '5', 'Testing 1'),
+  ('Google', 'https://www.google.com', '4', 'Test site 2'),
+  ('Pinterest', 'https://www.pinterest.com', '3', 'Test site 3');
